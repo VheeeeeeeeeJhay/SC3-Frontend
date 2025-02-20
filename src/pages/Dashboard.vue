16f63b394e4d2510bdeb997d8d05e2fd2c0e1093
ccandre <ccandremarin@gmail.com>
2025-02-20 09:11:14 +0700

    chart edit

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -6,12 +6,12 @@ import { useThemeStore } from '../stores/themeStore';
 // import PieChart from "../components/charts/PieChart.vue";
 // import LineChart from "../components/charts/LineChart.vue";
 // import BarChart from "../components/charts/BarChart.vue";
-const theme = ref(localStorage.getItem("theme") || "light");
 const themeStore = useThemeStore();
-const themeClasses = computed(() => 
-    theme.value === "dark" ? "bg-slate-600 border-black text-white" : "bg-white border-gray-200 text-gray-900"
-);
+const themeClasses = computed(() => {
+  return themeStore.isDarkMode ? "bg-slate-800 border-black text-white" : "bg-sky-50 border-gray-200 text-sky-900"
+})
 
+//piechart
 const getPieChartOptions = () => ({
   series: [52.8, 26.8, 20.4],
   colors: ["#1C64F2", "#16BDCA", "#9061F9"],
@@ -56,11 +56,12 @@ const getPieChartOptions = () => ({
 
 onMounted(() => {
   if (document.getElementById("pie-chart") && typeof ApexCharts !== 'undefined') {
-    const chart = new ApexCharts(document.getElementById("pie-chart"), getPieChartOptions());
-    chart.render();
+    const piechart = new ApexCharts(document.getElementById("pie-chart"), getPieChartOptions());
+    piechart.render();
   }
 });
 
+//linechart
 const options = {
   chart: {
     height: "100%",
@@ -141,19 +142,133 @@ if (document.getElementById("line-chart") && typeof ApexCharts !== 'undefined') 
 }
 });
 
+//barchart
+
+const barOptions = {
+  colors: ["#1A56DB", "#FDBA8C"],
+  series: [
+    {
+      name: "Organic",
+      color: "#1A56DB",
+      data: [
+        { x: "Mon", y: 231 },
+        { x: "Tue", y: 122 },
+        { x: "Wed", y: 63 },
+        { x: "Thu", y: 421 },
+        { x: "Fri", y: 122 },
+        { x: "Sat", y: 323 },
+        { x: "Sun", y: 111 },
+      ],
+    },
+    {
+      name: "Social media",
+      color: "#FDBA8C",
+      data: [
+        { x: "Mon", y: 232 },
+        { x: "Tue", y: 113 },
+        { x: "Wed", y: 341 },
+        { x: "Thu", y: 224 },
+        { x: "Fri", y: 522 },
+        { x: "Sat", y: 411 },
+        { x: "Sun", y: 243 },
+      ],
+    },
+  ],
+  chart: {
+    type: "bar",
+    height: "320px",
+    fontFamily: "Inter, sans-serif",
+    toolbar: {
+      show: false,
+    },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: "70%",
+      borderRadiusApplication: "end",
+      borderRadius: 8,
+    },
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    style: {
+      fontFamily: "Inter, sans-serif",
+    },
+  },
+  states: {
+    hover: {
+      filter: {
+        type: "darken",
+        value: 1,
+      },
+    },
+  },
+  stroke: {
+    show: true,
+    width: 0,
+    colors: ["transparent"],
+  },
+  grid: {
+    show: false,
+    strokeDashArray: 4,
+    padding: {
+      left: 2,
+      right: 2,
+      top: -14
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    floating: false,
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "Inter, sans-serif",
+        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+      }
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+}
+
+
+onMounted(() => {
+  if(document.getElementById("column-chart") && typeof ApexCharts !== 'undefined') {
+    const barchart = new ApexCharts(document.getElementById("column-chart"), barOptions);
+    barchart.render();
+  }
+});
 
 
 </script>
 
 <template>
-  <div :class="themeClasses" class="min-h-screen">
+  <div class="min-h-screen">
 
 
     <main class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <div class="grid grid-cols-3 gap-6">
-        <div class="col-span-1 bg-white p-6 rounded-lg shadow dark:bg-gray-800" :class="themeClasses">
+        <div class="col-span-1 p-6 rounded-lg shadow" :class="themeClasses">
           <div class="flex justify-between items-center mb-4" >
-            <h2 class="text-xl font-semibold" >Distribution of Incidents</h2>
+            <h2 class="text-xl font-semibold" :class="themeClasses">Distribution of Incidents</h2>
             <button class="px-3 py-1 text-sm bg-blue-500 text-white rounded hover:bg-blue-600">
               Date Filter
             </button>
@@ -161,20 +276,27 @@ if (document.getElementById("line-chart") && typeof ApexCharts !== 'undefined') 
           <div id="pie-chart" class="h-64"></div>
         </div>
         
-        <div class="col-span-1 bg-white p-6 rounded-lg shadow dark:bg-gray-800">
+        <div class="col-span-1 p-6 rounded-lg shadow" :class="themeClasses">
           <h2 class="text-xl font-semibold mb-4" :class="themeClasses">Recent Activities</h2>
           <!-- Add content for key metrics -->
         </div>
         
-        <div class="col-span-1 bg-white p-6 rounded-lg shadow dark:bg-gray-800 h-32">
+        <div class="col-span-1 p-6 rounded-lg shadow" :class="themeClasses">
           <h2 class="text-xl font-semibold mb-4" :class="themeClasses">Growth Rate of Incidents </h2>
           <!-- Add content for recent activities -->
+           <h2>two buttons for date comparison</h2>
+          <div class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
+            23%
+            <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
+            </svg>
+          </div>
         </div>
       </div>
       
       <div class="mt-6 grid grid-cols-2 gap-6">
         <!-- linechart -->
-        <div class="bg-white p-6 rounded-lg shadow dark:bg-gray-800">
+        <div class="p-6 rounded-lg shadow" :class="themeClasses">
             <div class="flex justify-between items-center mb-4">
               <h2 class="text-xl font-semibold" :class="themeClasses">Crime Statistics</h2>
               <button class="px-3 py-1 text-sm bg-blue-500 text-white rounded hover:bg-blue-600">
@@ -185,14 +307,14 @@ if (document.getElementById("line-chart") && typeof ApexCharts !== 'undefined') 
         </div>
       
       
-          <div class="bg-white p-6 rounded-lg shadow dark:bg-gray-800">
+          <div class="p-6 rounded-lg shadow" :class="themeClasses">
               <div class="flex justify-between items-center mb-4">
-                <h2 class="text-xl font-semibold" :class="textClass">Number of Incidents</h2>
+                <h2 class="text-xl font-semibold" :class="themeClasses">Number of Incidents</h2>
                 <!-- <button class="px-3 py-1 text-sm bg-blue-500 text-white rounded hover:bg-blue-600">
                   Date Filter
                 </button> -->
               </div>
-            <!-- <div id="line-chart" class="h-96"></div> -->
+              <div id="column-chart"></div>
           </div>
       </div>
     </main>
