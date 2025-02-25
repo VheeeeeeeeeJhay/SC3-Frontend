52edc3d90297d2f1bba7697c19b5733bcb7396c5
ccandre <ccandremarin@gmail.com>
2025-02-25 15:24:18 +0700

    map infinity values fixed

--- src/stores/mapStore.js
+++ src/stores/mapStore.js
@@ -1,8 +1,8 @@
 import { useLocalStorage } from '@vueuse/core';
 
 export const userMarker = useLocalStorage("USER_MARKER", {
-    latitude: 0,
-    longitude: 0,
+    latitude: 16.41,
+    longitude: 120.59,
   });
   
 export const nearbyMarkers = useLocalStorage("NEARBY_MARKERS", [1]);
