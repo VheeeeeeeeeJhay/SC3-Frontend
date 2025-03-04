fc4a01a8d37f9406b2b4ca4a13b8f962c7b09b7f
VheeeeeeeeeJhay <vheebhee7@gmail.com>
2025-03-04 07:54:57 +0700

    fix: routing for report table

--- src/pages/report/ReportsTable.vue
+++ src/pages/report/ReportsTable.vue
@@ -219,7 +219,7 @@ const formSubmit = async (report_Id) => {
                     </div>
                     <div
                         class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                        <RouterLink :to="{ name: 'ReportForm' }">
+                        <RouterLink :to="{ name: 'AddReport' }">
                             <button type="button"
                                 class="flex items-center justify-center font-medium rounded-lg text-sm px-3 py-1"
                                 :class="hoverClasses">
