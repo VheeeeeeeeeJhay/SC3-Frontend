57e7f3a979300a5e1fac24e77407dddb4e086dd1
VheeeeeeeeeJhay <vheebhee7@gmail.com>
2025-03-13 11:49:49 +0800

    refactor: redirect to report table after update

--- src/components/tabs/ArchiveTabs.vue
+++ src/components/tabs/ArchiveTabs.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue';
 import axiosClient from  '../../axios.js';
-import { RouterLink } from 'vue-router';
 import Badge from '../../components/Badge.vue';
 
 const users = ref([]);
