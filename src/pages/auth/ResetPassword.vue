53fee0dff48adc67ef79a01b7fa934212ea7f094
VheeeeeeeeeJhay <vheebhee7@gmail.com>
2025-04-22 15:02:33 +0800

    refactor: bg for reset-password

--- src/pages/auth/ResetPassword.vue
+++ src/pages/auth/ResetPassword.vue
@@ -58,51 +58,55 @@ const submitNewPassword = async () => {
   }">
 
     <!-- Main Container -->
-    <div class="flex flex-col sm:flex-row items-center gap-8 z-10">
-      <!-- City Logo and Title -->
-      <div class="text-center">
-        <img :src="logo" alt="City of Baguio Logo" class="w-70 mx-auto mb-4" />
-        <h1 class="text-white text-2xl font-bold leading-tight">
-          Smart City Command<br />
-          and Control Center<br />
-          <i>911 Dashboard</i>
-        </h1>
-      </div>
+    <div class="w-full px-4 sm:px-8 py-8 bg-black/50 backdrop-blur-md  shadow-inner">
+      <div class="flex flex-col sm:flex-row justify-center items-center gap-8 max-w-6xl mx-auto">
+        <!-- City Logo and Title -->
+        <div class="text-center">
+          <img :src="logo" alt="City of Baguio Logo" class="w-70 mx-auto mb-4" />
+          <h1 class="text-white text-2xl font-bold leading-tight">
+            Smart City Command<br />
+            and Control Center<br />
+            <i>911 Dashboard</i>
+          </h1>
+        </div>
 
-      <!-- Reset Password Email Form -->
-      <div class="bg-white p-6 rounded-xl shadow-lg w-80">
-        <h2 class="text-gray-800 text-lg text-center font-semibold mb-4">
-          Reset Password
-        </h2>
-
-        <form @submit.prevent="submitNewPassword" class="flex flex-col gap-4">
-          <div>
-            <label for="password" class="block text-sm font-medium text-gray-700">New Password</label>
-            <input type="password" id="password" v-model="dataNewPassword.password"
-              class="mt-1 w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            <p class="text-sm mt-1 text-red-600">
-              {{ errorsNewPassword.password ? errorsNewPassword.password[0] : '' }}
-            </p>
-          </div>
-          <div>
-            <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
-            <input type="password" id="password_confirmation" v-model="dataNewPassword.password_confirmation"
-              class="mt-1 w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            <p class="text-sm mt-1 text-red-600">
-              {{ errorsNewPassword.password_confirmation ? errorsNewPassword.password_confirmation[0] : '' }}
-            </p>
-          </div>
-          <button type="submit" class="w-full bg-teal-600 text-white py-2 rounded-md hover:bg-teal-700">
+        <!-- Reset Password Email Form -->
+        <div class="bg-white p-6 rounded-xl shadow-lg w-80">
+          <h2 class="text-gray-800 text-lg text-center font-semibold mb-4">
             Reset Password
-          </button>
-        </form>
-        <div class="text-sm text-center mt-4">
-          <RouterLink :to="{ name: 'Login' }" class="text-blue-600 hover:underline">
-            Return to login
-          </RouterLink>
+          </h2>
+
+          <form @submit.prevent="submitNewPassword" class="flex flex-col gap-4">
+            <div>
+              <label for="password" class="block text-sm font-medium text-gray-700">New Password</label>
+              <input type="password" id="password" v-model="dataNewPassword.password"
+                class="mt-1 w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+              <p class="text-sm mt-1 text-red-600">
+                {{ errorsNewPassword.password ? errorsNewPassword.password[0] : '' }}
+              </p>
+            </div>
+            <div>
+              <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm
+                Password</label>
+              <input type="password" id="password_confirmation" v-model="dataNewPassword.password_confirmation"
+                class="mt-1 w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+              <p class="text-sm mt-1 text-red-600">
+                {{ errorsNewPassword.password_confirmation ? errorsNewPassword.password_confirmation[0] : '' }}
+              </p>
+            </div>
+            <button type="submit" class="w-full bg-teal-600 text-white py-2 rounded-md hover:bg-teal-700">
+              Reset Password
+            </button>
+          </form>
+          <div class="text-sm text-center mt-4">
+            <RouterLink :to="{ name: 'Login' }" class="text-teal-600 hover:underline">
+              Return to login
+            </RouterLink>
+          </div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
